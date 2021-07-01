80865d7b5ea24c1967e3f6ff0b04a000e07511e4
ivorytone <jay.dnb@outlook.in>
2021-07-01 08:16:33 +0530

    Fix bug in logout command

--- src/ch_boot/terminate.py
+++ src/ch_boot/terminate.py
@@ -1,6 +1,8 @@
 from ch_boot.startup import *
+from ch_discord_utils.embed_generator import meta_message
 
 @charity.listen("on_message")
 async def on_terminate(message):
     if message.author.id == 799186130654199809 and message.content == ";logout":
+        message.reply(embed = meta_message(description = meta_message(description = "**`Shard logging out...`**")))
         exit()
