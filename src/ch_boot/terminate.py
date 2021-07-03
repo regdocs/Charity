e910eb4ea238f823e87bc24c5a3c8b4bfb143e4a
ivorytone <jay.dnb@outlook.in>
2021-07-04 00:46:30 +0530

    Rename file name to ccmd

--- src/ch_boot/terminate.py
+++
@@ -1,8 +0,0 @@
-from ch_boot.startup import *
-from ch_discord_utils.embed_generator import meta_message
-
-@charity.listen("on_message")
-async def on_terminate(message):
-    if message.author.id == 799186130654199809 and message.content == ";logout":
-        await message.reply(embed = meta_message(description = "**`Shard logging out...`**"))
-        exit()
