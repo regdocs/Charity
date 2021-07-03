5ea051f27bb7f1c0ab66f677b3d26e6a1583f798
ivorytone <jay.dnb@outlook.in>
2021-07-03 22:32:23 +0530

    Add task to reassign taken roles after unmute

--- src/ch_moderation/unmute.py
+++ src/ch_moderation/unmute.py
@@ -27,10 +27,6 @@ async def unmute(ctx, member: discord.Member, *, message_arg: str):
                 t = []
                 for i in x["r@ini_tse"]:
                     t.append(ctx.guild.get_role(i))
-                nitro_booster_role = ctx.guild.premium_subscriber_role
-                if nitro_booster_role in t:
-                    t.remove(nitro_booster_role)
-                print(t)
                 await member.add_roles(*t)
     tse = time.time()
     clc_usrinfract.update_one(
