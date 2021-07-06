14fd72882510022fb2e80eecfa04011dbaad6bd7
ivorytone <jay.dnb@outlook.in>
2021-07-06 17:21:16 +0530

    Update reaction target

--- src/ch_solaris/momma.py
+++ src/ch_solaris/momma.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 
 @charity.listen("on_message")
 async def react_on_mommas_oranges(message):
-  if "<:orange:" in message.content and (message.author.id == 805108723387334657 or message.author.id == 799186130654199809):
+  if "<:orange:" in message.content and (message.author.id == 805108723387334657):
     humble_abode = await charity.fetch_guild(829811466906632213)
     await message.add_reaction(await humble_abode.fetch_emoji(841124452283580417))
     return
