faa8aa0721260eb5453aad19f216fc310463725f
jay-io <jay.dnb@outlook.in>
2022-01-08 12:52:13 +0530

    Charity is now open source under the GPL License

--- src/ch_boot/cmongodb.py
+++ src/ch_boot/cmongodb.py
@@ -1,10 +1,12 @@
 import pymongo
+import os
 from pymongo import MongoClient
 
-mongoclient = MongoClient(r"mongodb+srv://jayantapandit:JI%26%2ACJ%25AAmongodb871@cluster0.5bm98.mongodb.net/test")
+mongoclient = MongoClient(os.getenv('MONGODB_TOKEN'))
 charity_alpha_mdb = mongoclient['charity-alpha']
 
 clc_afk = charity_alpha_mdb['away-from-keyboard']
 clc_gconfig = charity_alpha_mdb['server-config']
 clc_usrinfract = charity_alpha_mdb['user-infractions']
 clc_starboard = charity_alpha_mdb['starboard-db']
+clc_modmail = charity_alpha_mdb['modmail-active-tickets']
