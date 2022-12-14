4c872b3df64bcb7ab02bf64c771b93749923a817
Jayanta Pandit <jay.dnb@outlook.in>
2022-12-14 11:44:06 +0530

    Update constants.py

--- src/ch_env/constants.py
+++ src/ch_env/constants.py
@@ -1,7 +1,7 @@
 import pymongo
 from pymongo import MongoClient
 
-mongoclient = MongoClient(r"mongodb+srv://jayantapandit:JI%26%2ACJ%25AAmongodb871@cluster0.5bm98.mongodb.net/test")
+mongoclient = MongoClient(os.getenv('MONGODB_TOKEN'))
 charity_alpha_mdb = mongoclient['charity-alpha']
 clc_gconfig = charity_alpha_mdb['server-config']
 
