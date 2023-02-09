c38637a6f40ea346ba3e80d41452b5708e51c8e2
jayzsh <jay.dnb@outlook.in>
2023-02-09 22:26:31 +0530

    Update hello.py

--- src/ch_fun/hello.py
+++ src/ch_fun/hello.py
@@ -4,7 +4,7 @@ from ch_boot.startup import *
 
 @charity.command(name = "hello")
 async def say_hello(ctx, arg):
-    elif ctx.author.id == 799186130654199809:
+    if ctx.author.id == 799186130654199809:
         await ctx.reply("Hey Dad! <:heartz:844352117674082305>")
     elif ctx.author.id == 819439855880372246:
         await ctx.reply("Hello son of WhiteFang of Hidden Leaf, Disciple of YellowFlash, Sixth Hokage of Leaf, The Copy Ninja, Hatake Kakashi of Sharingan! <:drake_yes:830863359716229160>")
